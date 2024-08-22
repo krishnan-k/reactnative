624330cb54ed1cc542f1ca6ef691f754d2698bec
krishnan <158058589+krishnan-k@users.noreply.github.com>
2024-08-22 18:03:34 +0530

    22-08-2024

--- component/Carousel.js
+++ component/Carousel.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Image, StyleSheet,View } from 'react-native'
+import Swiper from 'react-native-swiper'
+const greenCard = require("../assets/greencard.png");
 
 const Carousel = () => {
   return (
-    <div>
-      
-    </div>
+    <Swiper loop={false}>
+      <View>
+        <Image source={greenCard} alt="greencard"/>
+      </View>
+      <View>
+        <Image source={greenCard} alt="greencard"/>
+      </View>
+      <View>
+        <Image source={greenCard} alt="greencard"/>
+      </View>
+    </Swiper>
   )
 }
+export default Carousel;
 
-export default Carousel
+const styles = StyleSheet.create({
+  
+})
