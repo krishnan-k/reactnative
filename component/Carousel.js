5942398e81ab31bb495a7557fe0932d94ff4ec09
krishnan <158058589+krishnan-k@users.noreply.github.com>
2024-08-22 18:18:00 +0530

    carousel

--- component/Carousel.js
+++ component/Carousel.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Image, StyleSheet,View } from 'react-native'
 import Swiper from 'react-native-swiper'
-const greenCard = require("../assets/greencard.png");
+const slide1 = require("../assets/7.jpg");
+const slide2 = require("../assets/9.jpg");
+const slide3 = require("../assets/10.jpg");
+const slide4 = require("../assets/6.jpg");
 
 const Carousel = () => {
   return (
     <Swiper loop={false}>
-      <View>
-        <Image source={greenCard} alt="greencard"/>
+      <View style={styles.slide}>
+        <Image style={styles.image} source={slide1} alt="slide1"/>
       </View>
-      <View>
-        <Image source={greenCard} alt="greencard"/>
+      <View style={styles.slide}>
+        <Image style={styles.image} source={slide2} alt="greencard"/>
       </View>
-      <View>
-        <Image source={greenCard} alt="greencard"/>
+      <View style={styles.slide}>
+        <Image style={styles.image} source={slide3} alt="greencard"/>
+      </View>
+      <View style={styles.slide}>
+        <Image style={styles.image} source={slide4} alt="greencard"/>
       </View>
     </Swiper>
   )
@@ -21,5 +27,13 @@ const Carousel = () => {
 export default Carousel;
 
 const styles = StyleSheet.create({
-  
+  image:{
+    // width: 100,
+    flex:1,
+    resizeMode:'contain',
+    alignSelf: 'center'
+  },
+  slide:{
+    flex:1
+  }
 })
